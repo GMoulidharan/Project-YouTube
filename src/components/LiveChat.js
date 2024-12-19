6f80f32493b93f64d57c8b35c6704585472a2f51
GMoulidharan <moulidharansrv@gmail.com>
2024-12-19 16:39:21 +0530

    Added Live chat feature to Improve user Interaction

---
+++ src/components/LiveChat.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import ChatMessage from "./ChatMessage"
+import { useDispatch, useSelector } from "react-redux"
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomNames, makeRandomMessage } from "../utils/helper";
+
+const LiveChat = () => {
+    const [liveMessage, setLiveMessage] = useState("");
+    const dispatch = useDispatch();
+
+    const chatMessages = useSelector((store) => store.chat.messages);
+
+    useEffect(() =>{
+        const i = setInterval(() =>{
+            //API polling
+            // console.log("API calling");
+            dispatch(
+                addMessage({
+                name:generateRandomNames(),
+                message:makeRandomMessage(10) + "🚀",
+            }))
+        },1500);
+
+        return () => clearInterval(i);
+    },[])
+  return (
+    <>
+    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg shadow-lg overflow-y-scroll flex flex-col-reverse">
+      <div>
+       {//Not use indexes as Key
+       chatMessages.map((c, index) =><ChatMessage
+        key={index} 
+        name={c.name}
+        message={c.message}
+       />)
+       }
+      </div>      
+    </div>
+
+    <form 
+      className="w-full p-2 m-2 border border-black rounded-md" 
+      onSubmit={(e) =>{
+        e.preventDefault();
+        console.log("ON submit form", liveMessage);
+        dispatch(addMessage({
+          name:"Mouli",
+          message:liveMessage,
+        }));
+        setLiveMessage("");
+      }}>
+      <input 
+      className="w-96 p-2" 
+      type="text" 
+      placeholder="Enter your text"
+      value={liveMessage}
+      onChange={(e) =>{
+        setLiveMessage(e.target.value);
+      }}
+      />
+      <button className="px-3 mx-4 bg-green-400 rounded-lg">Send</button>
+    </form>
+    </>
+  )
+}
+
+export default LiveChat
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
 import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
   // console.log(searchParams.get("v"));
@@ -12,8 +13,9 @@ const WatchPage = () => {
   }, []);
   return (
     <>
-    <div className="flec flex-col">
-      <div className="px-5">
+    <div className="flec flex-col w-full">
+      <div className="px-5 flex w-full">
+        <div>
         <iframe
           width="1200"
           height="600"
@@ -23,6 +25,10 @@ const WatchPage = () => {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
+        </div>
+        <div className="w-full">
+          <LiveChat />
+        </div>
       </div>
       <CommentsContainer />
       </div>
